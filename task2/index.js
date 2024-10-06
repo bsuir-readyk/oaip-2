407875da5ac5d52e3fa775f753910c84f698990b
readyyyk <mr.sairexer@gmail.com>
2024-10-07 01:34:44 +0300

    task2 in js

--- task2/index.js
+++ task2/index.js
@@ -1,4 +1,42 @@
 // nroot(1 + cos((x π) / n) ^ 2,3) Sum(e^(2 / 3 - k) / sqrt(x^k + x^(2k - 1)), k, 1, n)
 
+const solve = (n, x) => {
+  if (x <= 0) { throw "[error]"; }
+
+  let k = 1;
+  let xk = x,
+    x2k1 = x,
+    sumRes = 0;
+  while (k <= n) {
+    sumRes += Math.exp(2 / 3 - k) / Math.sqrt(xk + x2k1);
+    xk *= x;
+    x2k1 *= x * x;
+    k++;
+  }
+  const cosRes = cos(x * Math.PI / n);
+  return Math.exp(Math.log(1 + cosRes * cosRes) / 3) * sumRes;
+}
+
+
+const cos = (x) => {
+  let cur = 1;
+  let xn = x * x;
+  let factn = 2;
+  let cnt = 2;
+  let res = cur;
+  const E = 1 / 1e4;
+  while (Math.abs(cur - xn / factn) > E) {
+    cur = xn / factn;
+    res += cnt % 2 === 0 ? -cur : cur;
+    cnt++;
+    xn *= x;
+    factn *= cnt;
+  }
+  return res;
+}
+
+
+// console.log(cos(0.5), Math.cos(0.5));
+console.log(solve(10, 1));
 
 
