8ab605fc06e83b8a0f0852d59f0989c595954bb4
readyyyk <mr.sairexer@gmail.com>
2024-10-10 14:19:28 +0300

    delphi code done

--- task2/DELPHI.dpr
+++ task2/DELPHI.dpr
@@ -1,64 +1,51 @@
 Uses sysutils;
 
-function Cos(x: Double): Double;
 var
-  cur: Double;
-  xn: Double;
-  factn: Integer;
-  cnt: Integer;
-  res: Double;
-begin
-  cur := 1;
-  xn := x * x;
-  factn := 2;
-  cnt := 2;
-  res := cur;
+  x: Double;
+  n: Integer;
 
-  while Abs(cur - xn / factn) > 1/1e6 do
-  begin
-    cur := xn / factn;
-    if cnt mod 2 = 0 then
-      res := res - cnt
-    else
-      res := res + cnt;
-    cnt := cnt + 1;
-    xn := xn * x;
-    factn := factn * cnt;
-  end;
-
-  Cos := res;
-end;
-
-function Solve(x: Double; n: Integer): Double;
-var
   k: Integer;
   xk: Double;
   x2k1: Double;
   sumRes: Double;
   cosRes: Double;
 begin
-  WriteLn(Format('%.4g %.4g %.4g', [Cos(1), Cos(0), Cos(0.5)]));
-  Cos(0.5);
-  ReadLn;
+  x := 0.6;
 
-  k := 1;
-  xk := x;
-  x2k1 := x;
-  sumRes := 0;
-
-  while k <= n do
+  while x <= 1.1 + 1/1e6 do
   begin
-    sumRes := Exp(2/3 - k) / sqrt(xk + x2k1);
-    xk := xk * x;
-    x2k1 := x2k1 * x * x;
-    k := k + 1;
-  end;
+    sumRes := 0;
+    n := 10;
 
-  cosRes := Cos(x / n * PI);
-  Result := Exp(Ln(1 + cosRes * cosRes) / 3) * sumRes;
+    while n <= 15 do
+    begin
+      k := 1;
+      xk := x;
+      x2k1 := x;
+
+      cosRes := Cos(x / n * PI);
+
+      if n = 10 then
+      begin
+        while k <= n do
+        begin
+          sumRes := sumRes + Exp(2/3 - k) / sqrt(xk + x2k1);
+          xk := xk * x;
+          x2k1 := x2k1 * x * x;
+          k := k + 1;
+        end
+      end
+      else
+      begin
+        sumRes := sumRes + Exp(2/3 - n) / sqrt(xk + x2k1);
+        xk := xk * x;
+        x2k1 := x2k1 * x * x;
+      end;
+     
+      WriteLn(Format('n=%d x=%.4g f=%.4g', [n, x, Exp(Ln(1 + cosRes * cosRes) / 3) * sumRes]));
+      n := n + 1; 
+    end;
+    x := x + 0.25;
 end;
-
-begin
-  Solve(0.5, 11);
 end.
 
--- task2/index.js
+++ task2/index.js
@@ -1,25 +1,6 @@
 // nroot(1 + cos((x π) / n) ^ 2,3) Sum(e^(2 / 3 - k) / sqrt(x^k + x^(2k - 1)), k, 1, n)
 
-const cos = (x) => {
-  let cur = 1;
-  let xn = x * x;
-  let factn = 2;
-  let cnt = 2;
-  let res = cur;
-  const E = 1 / 1e4;
-  while (Math.abs(cur - xn / factn) > E) {
-    cur = xn / factn;
-    res += cnt % 2 === 0 ? -cur : cur;
-    cnt++;
-    xn *= x;
-    factn *= cnt;
-  }
-  return res;
-}
-
-const solve = (n, x) => {
-  if (x <= 0) { throw "[error]"; }
-
+const solve = (x, n) => {
   let k = 1,
     xk = x,
     x2k1 = x,
@@ -30,14 +11,14 @@ const solve = (n, x) => {
     x2k1 *= x * x;
     k++;
   }
-  const cosRes = cos(x * Math.PI / n);
+  const cosRes = Math.cos(x * Math.PI / n);
   return Math.exp(Math.log(1 + cosRes * cosRes) / 3) * sumRes;
 }
 
 
+// console.log(cos(1), cos(0), cos(0.5));
+console.log(solve(0.5, 11));
 
-console.log(cos(1), cos(0), cos(0.5));
-// console.log(cos(0.5), Math.cos(0.5));
-console.log(solve(11, 0.6));
 
+let x = 0.6
 
